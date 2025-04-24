bbfb7e7da804682cedbda17e34f78372597d5ef2
javier-nogales <javier.nog76@gmail.com>
2025-04-24 23:24:07 +0200

    Refactor: reorganize repo structure

--- src/main/java/com/naizfit/app/config/AppModule.java
+++ src/main/java/com/naizfit/app/config/AppModule.java
@@ -3,11 +3,14 @@ package com.naizfit.app.config;
 import com.google.inject.AbstractModule;
 import com.google.inject.servlet.ServletModule;
 import com.naizfit.app.interfaceapi.ApiServlet;
+import com.naizfit.app.interfaceapi.PingController;
 import com.naizfit.app.interfaceapi.Router;
 
 public class AppModule extends AbstractModule {
+	
     @Override
     protected void configure() {
+    	
         install(new ServletModule() {
             @Override
             protected void configureServlets() {
@@ -15,7 +18,9 @@ public class AppModule extends AbstractModule {
             }
         });
         
+        // routes
         bind(Router.class);
+        bind(PingController.class);
     }
 }
 
---
+++ technical-test-plan.md
@@ -0,0 +1,13 @@
+## Technical Test Plan
+
+This document have ideas, decisions, tasks and reflexions during technical test development. 
+
+### Create proyect infrastructure
+- [x] Review documentation
+- [x] Create GitHub repo
+- [x] Create maven proyect
+- [x] Create minimal test with Jakarta servlet
+- [x] Configure Guice and create minimal test without JakartaServlet
+- [ ] Create JUnit test for http://localhost:8080/naizfit-tester-api/api/ping (reponse: pong)
+- [ ] Refactor: Reorganize github repo structure
+- [ ] Create Docker
